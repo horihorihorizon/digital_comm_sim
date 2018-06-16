956c0839e906b81d74e2666bfb43553bd9f887dc
horihorihorizon <horielectronics@gmail.com>
2018-06-16 23:47:36 +0900

    シミュレーションを直交検波まで延長

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import generator as gen
+import receiver as rx
+import numpy as np
+import signal as usrsig
+import scipy.signal as scpsig 
 from matplotlib import pyplot 
 
 Fs = 48e3
-Flo = 200
+Flo = 1000
 SR = 200
-Point = 4800
+Point = 19200
 
 [i, q] = gen.baseband(Fs, SR, Point)
 rf = gen.quadmod(Fs, Flo, i, q)
 
-pyplot.plot(i)
-pyplot.plot(q)
-pyplot.plot(rf)
+[mixout_i, mixout_q] = rx.quaddet(Fs, Flo, rf)
+[lpfout_i, lpfput_q] = rx.detlpf(Fs, Flo, mixout_i, mixout_q)
+
+pyplot.plot(lpfout_i)
+pyplot.plot(lpfput_q)
+#pyplot.plot(rf)
+pyplot.grid()
 pyplot.show()
